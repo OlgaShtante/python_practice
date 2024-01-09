765bc5dce6bdb683c583a4ef761ef866668ffeee
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2024-01-09 20:26:45 +0100

    update based on the Dzmitry's comment to make function shorter

--- my-homework/hw_4/task_3.py
+++ my-homework/hw_4/task_3.py
@@ -1,15 +1,14 @@
 # create a function to check if year is leap/non-leap
 def is_leap_year(year):
     # check if the year is within a valid range
+    if not (1 <= year <= 2024):
+        return "Введено неверное значение! Введите год от 1 до 2024."
     if 1 <= year <= 2024:
         # check if the year is a leap year
         if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
             return "Високосный"
         else:
             return "Не високосный"
-    # in case of incorrect input
-    else:
-        return "Введено неверное значение! Введите год от 1 до 2024."
 
 # get a year to check
 inserted_year = int(input("Введите год, чтобы узнать, является ли он високосным: "))
