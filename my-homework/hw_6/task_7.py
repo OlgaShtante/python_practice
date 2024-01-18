b40d1b976ea53232c6c5970fdd221b810714f77b
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2024-01-19 00:11:12 +0100

    add task_7 hw_6

---
+++ my-homework/hw_6/task_7.py
@@ -0,0 +1,39 @@
+def is_list_of_integers(input_list):
+    if not isinstance(input_list, list):
+        return False
+
+    if input_list == []:
+        return False
+
+    for item in input_list:
+        if not isinstance(item, int):
+            return False
+
+    return True
+
+def count_students_above_average(input_list):
+    if not is_list_of_integers(input_list):
+        print(f"Incorrect input {input_list}. Only list of integers is allowed")
+        return []
+
+    average_height = sum(input_list) / len(input_list)
+    students_qty_above_avg_height = sum(1 for height in input_list if height > average_height)
+
+    return students_qty_above_avg_height
+
+def test(test_data, exp_res):
+    act_res = count_students_above_average(test_data)
+    print(f"Test: {test_data} returns {act_res}")
+    assert act_res == exp_res, f"Error: actual result: {act_res} does not match expected result: {exp_res}"
+
+test_data = [182, 161, 175]
+exp_res = 2
+test(test_data, exp_res)
+
+test_data = [175, 175]
+exp_res = 0
+test(test_data, exp_res)
+
+test_data = []
+exp_res = []
+test(test_data, exp_res)
