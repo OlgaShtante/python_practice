1c950cc2472df581311ff27fc02f67c8fc085efd
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2024-01-19 00:46:18 +0100

    update task_7 to use its function in task_8

--- my-homework/hw_6/task_7.py
+++ my-homework/hw_6/task_7.py
@@ -1,8 +1,9 @@
+def is_list_empty(input_list):
+    if input_list == []:
+        return True
+
 def is_list_of_integers(input_list):
     if not isinstance(input_list, list):
-        return False
-
-    if input_list == []:
         return False
 
     for item in input_list:
@@ -12,6 +13,10 @@ def is_list_of_integers(input_list):
     return True
 
 def count_students_above_average(input_list):
+    if is_list_empty(input_list):
+        print("Empty list")
+        return []
+
     if not is_list_of_integers(input_list):
         print(f"Incorrect input {input_list}. Only list of integers is allowed")
         return []
@@ -21,19 +26,21 @@ def count_students_above_average(input_list):
 
     return students_qty_above_avg_height
 
+
 def test(test_data, exp_res):
     act_res = count_students_above_average(test_data)
     print(f"Test: {test_data} returns {act_res}")
     assert act_res == exp_res, f"Error: actual result: {act_res} does not match expected result: {exp_res}"
 
-test_data = [182, 161, 175]
-exp_res = 2
-test(test_data, exp_res)
+if __name__ == "__main__": #to avoid test execution when import function from this file to task_8.py
+    test_data = [182, 161, 175]
+    exp_res = 2
+    test(test_data, exp_res)
 
-test_data = [175, 175]
-exp_res = 0
-test(test_data, exp_res)
+    test_data = [175, 175]
+    exp_res = 0
+    test(test_data, exp_res)
 
-test_data = []
-exp_res = []
-test(test_data, exp_res)
+    test_data = []
+    exp_res = []
+    test(test_data, exp_res)
