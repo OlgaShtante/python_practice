3532862a9a09c91e1280ff75bd5844f518032294
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2024-01-20 20:32:46 +0100

    add task_4 hw_7

---
+++ my-homework/hw_7/task_4.py
@@ -0,0 +1,42 @@
+from task_2 import is_tuple
+from task_3 import is_empty
+
+def is_value_int(input_value):
+    for value in input_value:
+        if not isinstance(value, int):
+            return False
+    return True
+
+def sum_of_even_numbers(input_tuple):
+    if is_tuple(input_tuple) and is_value_int(input_tuple) and not is_empty(input_tuple, 'Tuple'):
+        even_sum = 0
+        for number in input_tuple:
+            if number % 2 == 0:
+                even_sum += number
+        print("Sum of even numbers:", even_sum)
+        return even_sum
+
+def test(test_data, exp_res):
+    act_res = sum_of_even_numbers(test_data)
+    print(f"Test: {test_data} returns {act_res}")
+    assert act_res == exp_res, f"Error: actual result: {act_res} does not match expected result: {exp_res}"
+
+# valid data
+test_data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+exp_res = 30
+test(test_data, exp_res)
+
+# tuple of odds
+test_data = (1, 3, 5, 7, 9)
+exp_res = 0
+test(test_data, exp_res)
+
+# empty tuple
+test_data = ()
+exp_res = None
+test(test_data, exp_res)
+
+# not a tuple
+test_data = [[], {"key":"value"}, {0,2}, 'str', 3.14, 100, None, False]
+for i in range(len(test_data)):
+    test(test_data[i], exp_res)
