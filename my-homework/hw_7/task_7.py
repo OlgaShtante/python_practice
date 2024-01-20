d3661e333a5aebedea624ade4121dabccc39026c
OlgaShtante <51260907+OlgaShtante@users.noreply.github.com>
2024-01-21 00:12:34 +0100

    add task_8 hw_7

--- my-homework/hw_7/task_7.py
+++ my-homework/hw_7/task_7.py
@@ -1,11 +1,27 @@
-def print_value_for_key():
-    try:
-        input_dict = eval(input("Enter dictionary (e.g., {'a': 1, 'b': 2, 'c': 3}): "))
-        input_key = input("Enter key: ")
-        return input_dict.get(input_key, "Key not found")
-    except Exception as error_msg:
-        print(f"Invalid input. Error: {error_msg}")
-        return None
+def print_value_for_key(dictionary, key):
+    return dictionary.get(key, "Key not found")
 
-result = print_value_for_key()
-print(result)
+try:
+    input_dict = eval(input("Enter dictionary, e.g., {'k1': 1, 'k2': 2}: "))
+    input_key = input("Enter key: ")
+    result = print_value_for_key(input_dict, input_key)
+    print(result)
+except Exception as error_msg:
+    print(f"Invalid input. Error: {error_msg}")
+
+def test(test_dict, test_key, exp_res):
+    act_res = print_value_for_key(test_dict, test_key)
+    print(f"Test: {test_dict}, {test_key} returns {act_res}")
+    assert act_res == exp_res, f"Error: actual result: {act_res} does not match expected result: {exp_res}"
+
+# valid data
+test_dict = {'a': 1, 'b': 2, 'c': 3}
+test_key = 'b'
+exp_res = 2
+test(test_dict, test_key, exp_res)
+
+# wrong key
+test_dict = {'a': 1, 'b': 2, 'c': 3}
+test_key = 'd'
+exp_res = "Key not found"
+test(test_dict, test_key, exp_res)
